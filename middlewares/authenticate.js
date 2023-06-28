c9ef2b91d314f86dac459857e4df69eee3114c81
Inna Kukla <0969125133@ukr.net>
2023-06-28 22:27:40 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,7 +6,7 @@ const { User } = require("../models/user");
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (!token || bearer !== "Bearer") {
     next(HttpErrors(401, "Not authorized"));
   }
   try {
