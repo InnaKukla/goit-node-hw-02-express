1748429a29c8d1c3dcf572bf1a734936ff131e38
Inna Kukla <0969125133@ukr.net>
2023-06-28 22:27:42 +0300

    Create auth.test.js

---
+++ controllers/auth.test.js
@@ -0,0 +1,40 @@
+
+// const express = require("express");
+// const { login } = require("./auth");
+// const { request } = require("http");
+// // const { before } = require("node:test");
+
+
+// const app = express();
+
+// describe("test login controller", async () => {
+//     before (() => app.listen(3000));
+//     // beforeAll(() => app.listen(3000));
+// // afterAll(() => app.close())
+//     it("login user", async () => {
+//         const response = await request(app).post("/api/users/login");
+//         console.log(response);
+  
+//     })
+// })
+
+
+
+
+
+
+// // eslint-disable-next-line no-undef
+// describe("test login function", () => {
+//     // eslint-disable-next-line no-undef
+//     it("login.res.status - 200)", () => {
+        
+//         // eslint-disable-next-line no-undef
+//         expect(login(login.res === 200)).toBe(true)
+//     });
+
+//     // it("res.status - 200)", () => {
+        
+        
+//     //     expect(login(res.status(200))).toBe(true)
+//     // })
+// })
