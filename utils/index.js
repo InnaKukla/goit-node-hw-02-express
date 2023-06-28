b34713f7f2988485b85cc4242590bf94d6e9a8d8
Inna Kukla <0969125133@ukr.net>
2023-06-28 22:28:03 +0300

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 const HttpError = require("./HttpError");
 const handleMongooseError = require("./handleMongooseError");
+const resizeAvatar = require("../utils/resizeAvatar")
 
 module.exports = {
   HttpError,
   handleMongooseError,
+  resizeAvatar
 };
