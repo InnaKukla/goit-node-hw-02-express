032012b9e6852b972a0856ff11dfeb1faf1c837c
Inna Kukla <0969125133@ukr.net>
2023-07-12 18:17:19 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,9 +3,8 @@ const { HttpError } = require("../utils");
 
 const isValidId = (req, res, next) => {
   const { _id } = req.body;
-  
+
   if (!isValidObjectId(_id)) {
-    console.log(_id);
     next(HttpError(400, `${_id} is not valid id `));
   }
   next();
